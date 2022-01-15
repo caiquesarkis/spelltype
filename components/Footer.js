9bcf9c479c846539114d006e26d4c6e3bf9a91b0
Caique <caique_sarkis_corebiz@whirlpool.com>
2022-01-15 19:32:13 -0300

    First commit

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <div className="text-center font-sans font-medium text-gray p-8">
+            Caique Sarkis @ 2021
+        </div>
+    )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import Head from "next/head"
+export default function Navbar(){
+    const pages = [
+        {
+            text: "Deal",
+            link: "/Game"
+        },
+        {
+            text: "Leaderboard",
+            link: "/Leaderboard"
+        },
+        {
+            text: "Login",
+            link: "/Login"
+        }
+    ]
+
+    return(
+        <>
+            <Head>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <link href="/dist/output.css" rel="stylesheet"></link>
+            </Head>
+            <ul className='list-none flex justify-around p-4 items-center bg-gray-light'>
+                <Link href="/">
+                    <a  className="text-blue bg-gray-dark font-sans font-bold px-16 py-5 text-base rounded-lg mr-48">Home</a>
+                </Link>
+                {pages.map((page,index)=>{
+                    return(
+                    <li key={index}>
+                        <Link href={page.link}>
+                            <a  className="text-gray-dark font-sans font-bold text-base">{page.text}</a>
+                        </Link>
+                    </li>
+                    )
+                })}
+                
+            </ul>
+        </>
+    )
+}
